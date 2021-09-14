4274a4ba59e13ed5c0d3b133ac2608b0ce1b333b
Treee <itsatreee.twitch@gmail.com>
2021-09-14 15:00:52 -0600

    updating multi ceaser to be actual vigenere cipher

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ function copyToClipboard() {
   document.execCommand("copy");
 }
 function populateResultElement(text) {
-  document.getElementById("result-text").value = text;
+  if (document.getElementById("result-text")) {
+    document.getElementById("result-text").value = text;
+  }
 }
 const BASE_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 // . = ° \u00B0 and - = ¬ \u00AC
@@ -112,6 +114,7 @@ function textToMorseCode(text, ditNdahMap, delimiter) {
     })
     .join(delimiter);
   populateResultElement(morseCode);
+  return morseCode;
 }
 
 function morseCodeToText(morseCode, ditNdahMap, delimiter) {
@@ -127,74 +130,63 @@ function morseCodeToText(morseCode, ditNdahMap, delimiter) {
     })
     .join(" ");
   populateResultElement(decodedText);
+  return decodedText;
 }
 
-// test scripts
-// console.log(textToMorseCode("sos sos sos"));
-// console.log(textToMorseCode("testing a sample string"));
+// positive shifts right -> negative shifts left <-
+function shiftSingleLetter(letter, shift, encode) {
+  let base_index = BASE_ALPHABET.indexOf(letter.toUpperCase());
+  let shift_index = BASE_ALPHABET.indexOf(shift.toUpperCase());
+  if (encode) {
+    shift_index *= -1;
+  }
+  let modified_shift_index = (base_index - shift_index) % BASE_ALPHABET.length;
+  // wrap it in the negative direction (i think abs() can fix this somehow)
+  if (modified_shift_index < 0) {
+    modified_shift_index = BASE_ALPHABET.length + modified_shift_index;
+  }
+  // apply the shift to get the actual character
+  const shifted_letter = BASE_ALPHABET[modified_shift_index];
+  // console.log(
+  //   `Base Index: ${base_index} | Shift Index: ${shift_index} | Modified Shift : ${modified_shift_index} | Shifted letter: ${shifted_letter}`
+  // );
+  return shifted_letter;
+}
 
-// console.log(
-//   morseCodeToText(
-//     "^ # ### ^ ## ^# ^^#|#^|### #^ ^^ #^^# #^## #|### ^ #^# ## ^# ^^#"
-//   )
-// );
-
-// const text_to_cipher = "RNM VTMS CTRSMU WMOL LRNM WO ZHCMHEDH TPH NMAT UHSADGH FQQD BKE UDN WI HMUMMV IV KIA SAZDDQVE";
-// const text_to_cipher = "RNM VTMS CTRSMU WMOL LRNM WO ZHCMHEDH TPH NMAT UHSADGH UEHB TKM MDV OI PEUUEV IT KQS SIRDLIVM";
-// const text_to_cipher = "ONE STEP CLOSER WELL DONE TO RECEEEVE THE NEXT MESSAGZ MEET THE MAN OF HERMES AT HIS PARADISE";
-// const text_to_cipher = "COLLECTIVE OPERATION ICHOR ACTIVATE GATHER NBC MASK FILTER BLOOD BAG";
-// const text_to_cipher = "SYRINGE TEST KIT MEET AT THE HIGHEST PEAK WAIT FOR THE SIGNAL";
-// const text_to_cipher = "PORTLAND PROVIDES";
-// const text_to_cipher = "MEET THE MAN OF HERMES AT HIS PARADISE";
-// const encode = true;
-
-// let ceaser_cipher_shift_keys = "038";
-
-let answer = "";
 function multiAlphabetCeaserCipher(
   encode,
   text_to_cipher,
   ceaser_cipher_shift_keys
 ) {
   let cipher_shift_counter = 0; // this helps keep a distinct count so empty spaces don't mess up encryption/decryption
+  let rotating_ceaser_shift = 0;
   let answer = "";
   for (let i = 0; i < text_to_cipher.length; i++) {
     const base_index = BASE_ALPHABET.indexOf(text_to_cipher[i].toUpperCase());
     if (base_index > -1) {
       // based on the current encoded character, what is the ceaser cipher shift index to use
-      let rotating_ceaser_shift =
+      rotating_ceaser_shift =
         cipher_shift_counter % ceaser_cipher_shift_keys.length;
-      // select the index for the ceaser shifted alphabet to use
-      const ceaser_shifted_alphabet = parseInt(
-        ceaser_cipher_shift_keys[rotating_ceaser_shift]
-      );
 
-      // the ceaser shift index that maps to the base index (the % at the end wraps the selection) (this is how we get to the real message)
-      // added a switch to allow for encoding and decoding.
-      let modified_shift_index =
-        (base_index - ceaser_shifted_alphabet) % BASE_ALPHABET.length;
-      if (encode) {
-        modified_shift_index =
-          (base_index + ceaser_shifted_alphabet) % BASE_ALPHABET.length;
-      }
-      // wrap it in the negative direction (i think abs() can fix this somehow)
-      if (modified_shift_index < 0) {
-        modified_shift_index = BASE_ALPHABET.length + modified_shift_index;
-      }
-      // apply the shift to get the actual character
-      const shifted_letter = BASE_ALPHABET[modified_shift_index];
+      // select the index for the ceaser shifted alphabet to use
+      const ceaser_shifted_alphabet =
+        ceaser_cipher_shift_keys[rotating_ceaser_shift];
+
+      let shifted_letter = shiftSingleLetter(
+        text_to_cipher[i],
+        ceaser_shifted_alphabet,
+        encode
+      );
 
       answer = answer.concat(shifted_letter);
       cipher_shift_counter++;
-      // console.log(
-      //   `Index: ${i} Ceaser Shift: ${rotating_ceaser_shift} Ceaser Key: ${ceaser_shifted_alphabet} BaseIndex: ${base_index} Cipher Letter: ${text_to_cipher[i]} shifted_letter: ${shifted_letter} modified_shift_index: ${modified_shift_index}`
-      // );
     } else {
+      // pass through punctuation or anything not in the base alphabet
       answer = answer.concat(text_to_cipher[i]);
-      // console.log(`Ceaser Shift: ? Ceaser Key: ? BaseIndex: ${base_index} Cipher Letter: ' ' shifted_letter: ? modified_shift_index: ?`);
     }
   }
   populateResultElement(answer);
+  return answer;
 }
 
 function encryptClick() {
@@ -209,7 +201,7 @@ function encryptClick() {
     textToMorseCode(userText, normalMorseMap, normalMorseCodeWordDelimiter);
   } else if (encryptionType === "singlecaeser") {
     const multiCipherKey = document.getElementById("single-key-text").value;
-    multiAlphabetCeaserCipher(true, userText, multiCipherKey);
+    multiAlphabetCeaserCipher(true, userText, BASE_ALPHABET[multiCipherKey]);
   } else if (encryptionType === "multicaeser") {
     const multiCipherKey = document.getElementById("multi-key-text").value;
     multiAlphabetCeaserCipher(true, userText, multiCipherKey);
@@ -232,4 +224,119 @@ function decryptClick() {
     multiAlphabetCeaserCipher(false, userText, multiCipherKey);
   }
 }
-// console.log(answer);
+
+function checkValues(actualResult, expectedResult, origin) {
+  if (actualResult === expectedResult) {
+    console.log(`Test: Encryption Succeeded on ${origin} --> ${actualResult}`);
+  } else {
+    console.log(
+      `Test: Encryption Failed on ${origin} --> ${actualResult} \n Expected: ${expectedResult}`
+    );
+  }
+}
+
+function myTests() {
+  let originalMessage = "SOS";
+  let modifiedMessage = textToMorseCode(
+    originalMessage,
+    normalMorseMap,
+    normalMorseCodeWordDelimiter
+  );
+  checkValues(modifiedMessage, "... --- ...", originalMessage);
+
+  originalMessage = "SOS";
+  modifiedMessage = textToMorseCode(
+    originalMessage,
+    morseMap,
+    morseCodeWordDelimiter
+  );
+  checkValues(
+    modifiedMessage,
+    "\u00B0\u00B0\u00B0 \u00AC\u00AC\u00AC \u00B0\u00B0\u00B0",
+    originalMessage
+  );
+
+  originalMessage = "this is a test for my tool";
+  modifiedMessage = textToMorseCode(
+    originalMessage,
+    normalMorseMap,
+    normalMorseCodeWordDelimiter
+  );
+  checkValues(
+    modifiedMessage,
+    "- .... .. ... / .. ... / .- / - . ... - / ..-. --- .-. / -- -.-- / - --- --- .-..",
+    originalMessage
+  );
+
+  originalMessage =
+    "- .... .. ... / .. ... / .- / - . ... - / ..-. --- .-. / -- -.-- / - --- --- .-..";
+  modifiedMessage = morseCodeToText(
+    originalMessage,
+    normalMorseMap,
+    normalMorseCodeWordDelimiter
+  );
+  checkValues(
+    modifiedMessage,
+    "this is a test for my tool".toUpperCase(),
+    originalMessage
+  );
+
+  originalMessage = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  modifiedMessage = multiAlphabetCeaserCipher(true, originalMessage, "b");
+  checkValues(modifiedMessage, "BCDEFGHIJKLMNOPQRSTUVWXYZA", originalMessage);
+
+  originalMessage = "THIS MESSAGE SHOULD BE SHIFTED BY 3";
+  modifiedMessage = multiAlphabetCeaserCipher(true, originalMessage, "d");
+  checkValues(
+    modifiedMessage,
+    "WKLV PHVVDJH VKRXOG EH VKLIWHG EB 3",
+    originalMessage
+  );
+  originalMessage = "WKLV PHVVDJH VKRXOG EH VKLIWHG EB 3";
+  modifiedMessage = multiAlphabetCeaserCipher(false, modifiedMessage, "d");
+  checkValues(
+    modifiedMessage,
+    "THIS MESSAGE SHOULD BE SHIFTED BY 3",
+    originalMessage
+  );
+
+  originalMessage =
+    "ZOHYHPQ SLLV AKL HE...IVXY PLQ, AKYHL ZVPLQ; HZHUL...WOHF PHUJK AZV EF WDR. AKLB DLSO ZRVQ RQVZ DKF...WOULH NULDA ELDZWZ IVOSRD WOHPU ZLE...VPA MDSO, URUH DLSO SLCH.";
+  modifiedMessage = multiAlphabetCeaserCipher(false, originalMessage, "dh");
+  checkValues(
+    modifiedMessage,
+    "Wherein lies the ex...Four men, three women; aware...They march two by two. They will soon know why...Three great beasts follow their six...six fall, none will live.".toUpperCase(),
+    originalMessage
+  );
+
+  originalMessage =
+    "Wherein lies the ex...Four men, three women; aware...They march two by two. They will soon know why...Three great beasts follow their six...six fall, none will live.";
+  modifiedMessage = multiAlphabetCeaserCipher(true, originalMessage, "dh");
+  checkValues(
+    modifiedMessage,
+    "ZOHYHPQ SLLV AKL HE...IVXY PLQ, AKYHL ZVPLQ; HZHUL...WOHF PHUJK AZV EF WDR. AKLB DLSO ZRVQ RQVZ DKF...WOULH NULDA ELDZWZ IVOSRD WOHPU ZLE...VPA MDSO, URUH DLSO SLCH.",
+    originalMessage
+  );
+
+  originalMessage = "zvze cf wkgkuht d sicfc xhe qzbbru";
+  modifiedMessage = multiAlphabetCeaserCipher(
+    false,
+    originalMessage,
+    "gormund"
+  );
+  checkValues(
+    modifiedMessage,
+    "this is testing a multi key cipher".toUpperCase(),
+    originalMessage
+  );
+
+  originalMessage = "this is testing a multi key cipher";
+  modifiedMessage = multiAlphabetCeaserCipher(true, originalMessage, "gormund");
+  checkValues(
+    modifiedMessage,
+    "zvze cf wkgkuht d sicfc xhe qzbbru".toUpperCase(),
+    originalMessage
+  );
+}
+
+myTests();
